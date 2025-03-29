6b9323257a828e2cec8df582b0c440502d956b97
Danila Kosmynin <dkosmyninspb@mail.ru>
2025-03-29 22:54:57 +0300

    stable version

--- config.py
+++ config.py
@@ -1,48 +1,46 @@
-import logging
+# -*- coding: utf-8 -*-
+import os
 from dataclasses import dataclass
 from pathlib import Path
-from environs import Env
+from dotenv import load_dotenv
+
+
 
 PROJECT_DIR = Path(__file__).parent
 
 
 # Инициализация объекта Env и чтение конфигурации из файла .env
-env = Env()
-try:
-    env.read_env(Path(PROJECT_DIR, r'deployments/.env'))
-except OSError:
-    pass
-
+load_dotenv(Path('C://Users/11/PycharmProjects/SberParsingKBFU/deployments', '.env'))
 
 @dataclass
 class Config:
-    VLESS_CONFIGURATION_PATH: str = env("VLESS_CONFIGURATION_PATH")
-    VLESS_CONFIGURATION_PUBLIC_KEY: str = env("VLESS_CONFIGURATION_PUBLIC_KEY")
-    VLESS_CONFIGURATION_PATH_WIN: str = env("VLESS_CONFIGURATION_PATH_WIN")
-    VLESS_HOST_IP: str = env("VLESS_HOST_IP")
+    VLESS_CONFIGURATION_PATH: str = os.environ.get("VLESS_CONFIGURATION_PATH")
+    VLESS_CONFIGURATION_PUBLIC_KEY: str = os.environ.get("VLESS_CONFIGURATION_PUBLIC_KEY")
+    VLESS_CONFIGURATION_PATH_WIN: str = os.environ.get("VLESS_CONFIGURATION_PATH_WIN")
+    VLESS_HOST_IP: str = os.environ.get("VLESS_HOST_IP")
 
     LOGS_DIR = Path(PROJECT_DIR, "logs")
     APP_LOGS_FILE = Path(LOGS_DIR, "app.log")
 
-    VLESS_LOG_DIR: str = env("VLESS_LOG_DIR")
+    VLESS_LOG_DIR: str = os.environ.get("VLESS_LOG_DIR")
     VLESS_ACCESS_LOG_PATH: Path = Path(VLESS_LOG_DIR, "access.log")
     VLESS_ERROR_LOG_PATH: Path = Path(VLESS_LOG_DIR, "error.log")
 
-    SERVER_ALIAS = env("SERVER_ALIAS")
-    SERVER_IP = env("SERVER_IP")
+    SERVER_ALIAS = os.environ.get("SERVER_ALIAS")
+    SERVER_IP = os.environ.get("SERVER_IP")
 
-    CONTROL_AUTH_LOGIN = env("CONTROL_AUTH_LOGIN")
-    CONTROL_AUTH_PASS_HASH = env("CONTROL_AUTH_PASS_HASH")
+    CONTROL_AUTH_LOGIN = os.environ.get("CONTROL_AUTH_LOGIN")
+    CONTROL_AUTH_PASS_HASH = os.environ.get("CONTROL_AUTH_PASS_HASH")
 
-    SESSION_LIFETIME_MIN = env.int("SESSION_LIFETIME_MIN")
-    PRIVATE_KEY_FILE: Path = PROJECT_DIR.joinpath(env("PRIVATE_KEY_FILE"))
-    PUBLIC_KEY_FILE: Path = PROJECT_DIR.joinpath(env("PUBLIC_KEY_FILE"))
-    PRIVATE_KEY: str = PRIVATE_KEY_FILE.read_text().strip()
-    PUBLIC_KEY: str = PUBLIC_KEY_FILE.read_text().strip()
+    SESSION_LIFETIME_MIN = int(os.environ.get("SESSION_LIFETIME_MIN"))
+    PRIVATE_KEY_FILE: Path = PROJECT_DIR.joinpath(os.environ.get("PRIVATE_KEY_FILE"))
+    PUBLIC_KEY_FILE: Path = PROJECT_DIR.joinpath(os.environ.get("PUBLIC_KEY_FILE"))
+    #PRIVATE_KEY: str = PRIVATE_KEY_FILE.read_text().strip()
+    #PUBLIC_KEY: str = PUBLIC_KEY_FILE.read_text().strip()
 
-    REDIS_HOST_ADDR: str = env("REDIS_HOST_ADDR")
-    REDIS_PORT: int = env.int("REDIS_PORT")
-    REDIS_PASS: str = env("REDIS_PASS")
+    REDIS_HOST_ADDR: str = os.environ.get("REDIS_HOST_ADDR")
+    REDIS_PORT: int = int(os.environ.get("REDIS_PORT"))
+    REDIS_PASS: str = os.environ.get("REDIS_PASS")
     FASTAPI_PORT = 8000
 
-    FASTAPI_WORKERS_COUNT: int = env.int("FASTAPI_WORKERS_COUNT")
+    FASTAPI_WORKERS_COUNT: int = int(os.environ.get("FASTAPI_WORKERS_COUNT"))
--- source/api/__init__.py
+++ source/api/__init__.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from source.api.server.routes import router as server_router
 from source.api.service.routes import router as service_router
-from source.api.auth import router as auth_router
+
 
 
 def include_routers(app: FastAPI):
@@ -11,6 +11,6 @@ def include_routers(app: FastAPI):
     """
     app.include_router(service_router)
     app.include_router(server_router)
-    app.include_router(auth_router)
+
 
 
